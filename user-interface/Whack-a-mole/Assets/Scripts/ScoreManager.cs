70701baca7d0cb8e1e818cd678cd881803706b21
beyondtheinferno <40118538+beyondtheinferno@users.noreply.github.com>
2019-02-28 01:40:44 +0530

    Whack-a-Thanos Complete version. Fixed the score. Fixed button functions. To-Do: 1. Disable button after Thanos is whacked / enable in the next sprite change cycle 2. A How-to-play-game pop up

---
+++ user-interface/Whack-a-mole/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    // Just the score :)
+    public int score = 0;
+}
---
+++ user-interface/Whack-a-mole/Assets/Scripts/TimerandUpdate.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class TimerandUpdate : MonoBehaviour
+{
+    // Outer Script referencing
+    ScoreManager script;
+
+    // Timer Text
+    float countDown = 60f;
+    private GameObject timerText;
+    Text meh;
+
+    // Randomizer
+    float timer;
+    public int i;
+
+    // Sprites list and the sprite which is rendered
+    public Sprite[] spritesList;
+    Image spriteRendered;
+    public Sprite empty;
+
+
+
+    // Activated when button is clicked
+    public void TaskOnClick()
+    {
+        if (i == 0)
+        {
+            script.score += 1;
+            spriteRendered.sprite = empty;
+        }
+        //Debug.Log(script.score);
+    }
+
+
+
+    void Start()
+    {
+        //Reference of the script having the "score" variable
+        script = GameObject.Find("GameManager").GetComponent<ScoreManager>();
+
+        spriteRendered = GetComponent<Image>();
+        timerText = GameObject.Find("CountDownText");
+        meh = timerText.GetComponent<Text>();
+    }
+
+
+
+    void Update()
+    {
+        meh.text = "Time : " + Mathf.RoundToInt(countDown) + "s";
+        countDown -= Time.deltaTime;
+
+        if (countDown > 0)
+        {
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                // Adjusting difficulty
+                timer = 3f;
+                if (Mathf.RoundToInt(countDown) <= 40)
+                    timer = 2f;
+                else if (Mathf.RoundToInt(countDown) <= 20)
+                    timer = 1f;
+                else if (Mathf.RoundToInt(countDown) <= 10)
+                    timer = 0.8f;
+
+                // Random index generation to select any one sprite from the given 5
+                i = UnityEngine.Random.Range(0, spritesList.Length);
+                spriteRendered.sprite = spritesList[i];
+                spriteRendered.color = new Color(spriteRendered.color.r, spriteRendered.color.g, spriteRendered.color.b, 255f);
+            }
+        }
+
+        else
+            meh.text = "Final Score : " + script.score;
+    }
+}
